ccc1d0ada1f537c05adc6d589a2ca8625dcff16a
MrData <mrdata@crypto2.net>
2014-09-03 22:16:49 -0400

    Set addresses to start with a

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 50,
+        PUBKEY_ADDRESS = 83,
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 26,
         SCRIPT_ADDRESS_TEST = 196,
